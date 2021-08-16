2e2fd55bc42f6823dc31cc07cbee6847067c2c55
AngelDragoniusX <dialga1994@gmail.com>
2021-08-16 02:02:10 -0400

    add MONGODB_URI env

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
@@ -8,15 +9,22 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-app.use(require('./routes'));
+
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social', {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
+}).then(res => {
+  console.log('connected to DB!!!');
+
+  app.use(require('./routes'));
+
+  // Use this to log mongo queries being executed!
+  mongoose.set('debug', true);
+
+  app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
+
+}).catch(err => {
+  console.log('err connecting to DB', err);
 });
-
-// Use this to log mongo queries being executed!
-mongoose.set('debug', true);
-
-app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
